9a4259cf0774874f5997a835b7283683a7cd682d
PatrykSkw <91796584+PatrykSkw@users.noreply.github.com>
2023-07-13 20:23:25 +0100

    Add files via upload

---
+++ RandomAPI/Controllers/RandomController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RandomAPI.Controllers
+{
+    [ApiController]
+    [Route("api/random")]
+    public class RandomController : ControllerBase
+    {
+        private static readonly Random random = new();
+
+        [HttpGet]
+        public int GetRandomNumber(int start, int end)
+        {
+            // dodać walidację, start nie może być większe niż end
+            return random.Next(start, end + 1);
+        }
+    }
+}
---
+++ RandomAPI/Program.cs
@@ -0,0 +1,56 @@
+using Microsoft.OpenApi.Models;
+using RandomAPI.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "Standard Authorization header using the Bearer scheme. Example: \"bearer {token}\"",
+        In = ParameterLocation.Header,
+        Name = "XApiKey",
+        Type = SecuritySchemeType.ApiKey
+    });
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Name = "XApiKey",
+                In = ParameterLocation.Header,
+                Reference = new OpenApiReference
+                {
+                    Id = "Bearer",
+                    Type = ReferenceType.SecurityScheme
+                }
+            },
+            new List<string>()
+        }
+    });
+});
+builder.Services.AddAuthentication(option =>
+{
+    option.DefaultAuthenticateScheme = "Bearer";
+    option.DefaultScheme = "Bearer";
+    option.DefaultChallengeScheme = "Bearer";
+});
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseMiddleware<ApiKeyMiddleware>();
+
+app.UseAuthentication();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
